e8d7a8e492b08c4eb60f483295fc25b8533e7143
Patcharapon Boonsiri <45910480+johnnii-dev@users.noreply.github.com>
2023-10-28 13:41:05 +0700

    Create aliases.zsh

---
+++ .zsh/aliases.zsh
@@ -0,0 +1,14 @@
+# Alias
+# ---
+#
+alias k="kubectl"
+alias h="helm"
+alias tf="terraform"
+alias a="ansible"
+alias ap="ansible-playbook"
+alias ls="exa --icons --group-directories-first"
+alias ll="exa --icons --group-directories-first -la"
+
+alias grep='grep --color'
+# mac OS shortcuts
+alias code="open -a 'Visual Studio Code'"
